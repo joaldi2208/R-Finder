7bf1a2ff87c36ed27d5936c696fd55c012564947
Jonas Dietrich <joaldi.98@gmail.com>
2023-05-14 17:01:24 +0200

    more useful docstring

--- scripts/SMILES_connection.py
+++ scripts/SMILES_connection.py
@@ -7,12 +7,9 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl 
 
 
-# C1CCC(CC1)(C2=CC=C(C=C2)O)C3=CC=C(C=C3)O 
-# O is OH !!!! I didn't know
-
 
 def replaceRinSMILES(R_replacements, r_in_smiles):
-    
+    "replaces the R-Group in the predicted structures with the found smiles in the R-Group definition"""    
     for OCR_identifier, R_group in R_replacements:
 
         R_replaced_SMILES = r_in_smiles.replace(OCR_identifier, R_group)
@@ -20,10 +17,11 @@ def replaceRinSMILES(R_replacements, r_in_smiles):
 
 
 
-r_in_smiles = "C1CCC(CC1)(C2=CC=C(C=C2)R)C3=CC=C(C=C3)R)"
+if __name__ == "__main__":
+    r_in_smiles = "C1CCC(CC1)(C2=CC=C(C=C2)R)C3=CC=C(C=C3)R)"
 
-R_replacements = [("R", "MOM"), ("R", "OH"), ("R", "H")]
-replaceRinSMILES(R_replacements, r_in_smiles)
-        
+    R_replacements = [("R", "MOM"), ("R", "OH"), ("R", "H")]
+    replaceRinSMILES(R_replacements, r_in_smiles)
+            
     
     
